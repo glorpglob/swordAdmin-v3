d3f352fdee86787f19d2b9d70545353587c6712c
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-01-25 01:56:27 +1300

    Create Information.lua

---
+++ Sword Admin v3/SAv3/Information.lua
@@ -0,0 +1,92 @@
+--[[
+Commands:
+1. !kill [player/all/me] 
+2. !privateserver (toggle)
+3. !admin [player] 
+4. !unadmin [player] 
+5. !speed [player/all/me] [amount/value] 
+6. !msg [message] 
+7. !kick [player/me/all] [message] 
+8. !god [player/me/all]
+9. !countdown [value]
+10. !music [id]
+11. !ff [player/me/all]
+12. !unff [player/me/all]
+13. !clone [player/me/all]
+14. !health [player/me/all] [value]
+15. !bring [player/me/all]
+16. !explode [player/me/all]
+17. !sit [player/me/all]
+18. !unsit [player/me/all]
+19. !freeze [player/me/all]
+20. !unfreeze [player/me/all]
+21. !ban [player]
+22. !unban [full_player_name]
+23. !sword [player/me/all]
+24. !gear [player/me/all] [id
+25. !fire [player/me/all]
+27. !unfire [player/me/all]
+28. !btools [player/me/all]
+29. !shutdownserver
+30. !brightness [value/default]
+31. !sparkles [player/me/all]
+32. !unsparkles [player/me/all]
+33. !faceless [player/me/all]
+34. !headless [player/me/all]
+35. !jumppower [player/me/all] [value]
+36. !blind [player/me/all]
+37. !unblind [player/me/all]
+38. !smoke [player/me/all]
+39. !unsmoke [player/me/all]
+40. !cmds
+41. !fling [player/me/all]
+42. !insert [id]
+43. !char [player/me/all] [id]
+44. !slip [player/me/all]
+45. !invisible [player/me/all]
+46. !print [string]
+47. !fly [player/me/all]
+48. !float [player/me/all]
+49. !unfloat [player/me/all]
+50. !noclip [player/me/all] (toggle)
+51. !uninvisible [player/me/all]
+52. !print [message]
+
+Credits:
+- WideSteal321 // via RobloxStudio // all commands and code
+- F3X // via Models/Toolbox // btools object
+- Khol // via Models/Toolbox // fly script (fixed by Sword Admin)
+- KillerBrenden // via ScriptingHelpers // !noclip help
+Credits changed as of September 2020 due to a complete revamp
+
+Releases:
+v0.0.0 - v1.0.0 [January 2019 - October 2019] 100 Versions
+v1.0.0 - v2.0.0 [October 2019 - August 2020] 30 Versions
+v2.0.0 - v3.0.0 [August 2020 - October 2020] 360 Versions (359 are unreleased)
+
+Notable releases:
+v0.9.5 - End of Alpha
+V1.0.0 - End of Beta
+V1.1.2 - Largest update
+v2.0.0 - Revamp of settings
+v3.0.0 - Complete revamp
+
+SAERR_CODE(s) (Sword Admin Error Code(s)):
+1 - Countdown value is nil.
+2 - Failed to save admins.
+3 - Private server
+4 - Banned
+5 - Failed to save bans
+6 - nil is true/existent
+7 - Sword admin does not work in studio
+
+Gamepass/Premium Commands:
+1. !clone [player/all/me]
+2. !countdown [value]
+3. !ff [player/all/me]
+4. !unff [player/all/me]
+5. !speed [player/all/me] [value]
+6. !kill [player/all/me]
+7. !print [string]
+8. !teleport [player/all/me]
+--]]
