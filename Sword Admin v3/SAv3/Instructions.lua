b3652baddc0fddb1662cc669a335c082ed44118f
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-01-25 01:56:52 +1300

    Create Instructions.lua

---
+++ Sword Admin v3/SAv3/Instructions.lua
@@ -0,0 +1,20 @@
+--[[
+1. [Setting Up]:
+a. Open the Module Script named "Settings"
+b. Customise the settings!
+
+2. [Updating]
+a. Delete the model
+b. Re-insert the models
+
+3. [I got an error!]
+a. Check the following error codes below:
+1 - Countdown value is nil.
+2 - Failed to save admins.
+3 - Private server
+4 - Banned
+5 - Failed to save bans
+6 - nil is true/existent
+7 - Sword admin does not work in studio
+b. now you know why you got it!
+--]]
