aca8443210ebc805b549e5b18e2cee5130c76370
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-01-25 01:57:26 +1300

    Create Settings.lua

---
+++ Sword Admin v3/SAv3/Settings.lua
@@ -0,0 +1,26 @@
+                                                                                                                                   return {
+------------------------------
+admins = { 
+	"WideSteal321"; 
+	"UserName";
+	123456789;
+	"2ndgame_test";
+	900;
+	game.CreatorId; -- do not remove, this is you.  
+};
+flamingoadmin = false; -- is mrflimflam admin?
+------------------------------
+bans = { 
+	"someoneyouhate";
+	"abc";
+};
+------------------------------
+prefix = "!"; -- what is said before each command
+buggycommands = false; -- !rejoin and !insert
+------------------------------	
+premiumAdmin = false; -- can someone own premium for admin commands (info in information script)
+gamepassAdmin = true; -- can someone buy a gamepass for admin commands (info in information script)
+gamepassid = 7861992; -- gamepass id if above is true
+------------------------------
+
+                                                                                                                      }
